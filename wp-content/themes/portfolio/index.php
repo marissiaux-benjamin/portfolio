0167adb46eeb09760c9cb44a280c646d3b59d2d1
benjamin <benjamin.marissiaux@student.hepl.be>
2024-04-22 09:54:23 +0200

    new branch

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,1 +1,29 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php dw_asset('css/accueil.css'); ?>">
+</head>
+<body>
+<header>
+    <section>
+    <?php if (have_posts()): while (have_posts()): the_post(); // Ouverture de "The Loop" de Wordpress ?>
+        <h2><?= get_the_title(); ?></h2>
+        <?= get_the_content(); ?>
+    <?php endwhile; endif; ?>
+
+    </section>
+</header>
+<main>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
+
+
