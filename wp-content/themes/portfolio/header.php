936564fb306f4119853308d442d329ce1073e366
benjamin <benjamin.marissiaux@student.hepl.be>
2024-04-30 18:36:38 +0200

    first sections + header

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= dw_asset('css/reset.css'); ?>">
-    <link rel="stylesheet" href="<?= dw_asset('css/accueil.css'); ?>">
+    <link rel="stylesheet" href="<?= dw_asset('css/style.css'); ?>">
     <title>Accueil</title>
 </head>
 <body>
@@ -16,7 +16,14 @@
                  alt="Logo de Benjamin Marissiaux représentant un B et un M majustcule" height="40" width="40">
         </a>
     </h2>
-    <?php if (have_posts()): while (have_posts()): the_post(); ?>
-        <?= get_the_content() ?>
-    <?php endwhile; endif; ?>
+    <nav>
+        <ul>
+            <?php foreach (dw_get_navigation_links('main') as $link): ?>
+                <li class="main_nav_container_item">
+                    <a class="nav_icon no_text_decoration" href="<?= $link->url ?>"
+                       title="Aller vers la page <?= $link->label ?>"><?= $link->label ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
 </header>
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
 <main class="main">
+        <?= get_the_content() ?>
 </main>
+
 
 
 
