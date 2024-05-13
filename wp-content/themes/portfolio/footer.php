752786210a391f83594cac2163996b9a8d724e59
benjamin <benjamin.marissiaux@student.hepl.be>
2024-05-13 14:37:20 +0200

    footer-created

---
+++ wp-content/themes/portfolio/footer.php
@@ -0,0 +1,9 @@
+        <footer>
+            <ul class="social-icons">
+                <li><a href="https://instagram.com">insta</a></li>
+                <li><a href="https://github.com">github</a></li>
+                <!-- Ajoutez d'autres icônes de réseaux sociaux au besoin -->
+            </ul>
+        </footer>
+    </body>
+</html>
