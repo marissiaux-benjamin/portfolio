94f3334d712fe8ac04d3d13d5164c99dabac5b99
benjamin <benjamin.marissiaux@student.hepl.be>
2024-06-18 02:36:15 +0200

    removed animations because nothing without javascript

--- wp-content/themes/portfolio/contact.php
+++ wp-content/themes/portfolio/contact.php
@@ -22,28 +22,30 @@ get_header()
 <main class="main-mention">
     <?= get_the_content() ?>
 
-    <p class="main__instruction-form">Faites attention&nbsp;! Tous les champs du formulaire sont obligatoires.</p>
-    <div class="main__form-container">
+    <p class="main-mention__instruction-form">Faites attention&nbsp;! Tous les champs du formulaire sont
+        obligatoires.</p>
+    <div class="main-mention__form-container">
 
-        <form action="<?= home_url() . $_SERVER['REQUEST_URI'] ?>" method="POST" class="main__form-container__form">
-            <input autocomplete="off" name="name" type="text" id="name" class="main__form-container__form__input-name" placeholder="prenom et nom" >
-            <label for="name" class="main__form-container__form__label-name">Prénom et nom *</label>
+        <form action="<?= home_url() . $_SERVER['REQUEST_URI'] ?>" method="POST"
+              class="main-mention__form-container__form">
+            <input type="text" id="nom" name="nom" class="main-mention__form-container__form__input-name"
+                   placeholder="Ex: Benjamin, Benjamin Marissiaux, Marissiaux,...">
+            <label for="nom" class="main-mention__form-container__form__label-name">Nom *</label>
 
-            <input name="mail" type="email" id="mail" class="main__form-container__form__input-mail"
+            <input name="mail" type="text" id="mail" class="main-mention__form-container__form__input-mail"
                    placeholder="ex: ben.maru@gmail.com">
-            <label for="mail" class="main__form-container__form__label-mail">Votre email *</label>
-
+            <label for="mail" class="main-mention__form-container__form__label-mail">Votre email *</label>
 
             <textarea name="message" id="message" cols="30" rows="10"
-                      class="main__form-container__form__input-text"
-                      placeholder="Votre message" ></textarea>
-            <label for="message" class="main__form-container__form__label-text">Votre message *</label>
-            <input name="soumettre" type="submit" value="Envoyer" class="main__form-container__form__btn">
+                      class="main-mention__form-container__form__input-text"
+                      placeholder="Votre message"></textarea>
+            <label for="message" class="main-mention__form-container__form__label-text">Votre message *</label>
+
+            <input name="soumettre" type="submit" value="Envoyer" class="main-mention__form-container__form__btn">
         </form>
     </div>
 </main>
-<?php var_dump(home_url() . $_SERVER['REQUEST_URI']) ?>
-<?php get_footer(); ?>
+<?php get_footer() ?>
 
 
 
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= dw_asset('css/reset.css'); ?>">
     <link rel="stylesheet" href="<?= dw_asset('css/style.css'); ?>">
+
     <title><?= get_the_title() ?></title>
 </head>
 <body>
